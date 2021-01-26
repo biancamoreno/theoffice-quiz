4f96ba5b7cc92c26ad531625997402fa41f69992
Bia <bianca.morenogar@gmail.com>
2021-01-26 18:52:11 -0300

    fix url param error

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 export default function QuizPage() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const name = urlParams.get('name');
-
     return (
-        <div>Página Quiz {name}</div>
+        <div>Página Quiz</div>
     )
 }
